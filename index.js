1fc6031c6f5ad5473c71ce2a2074ae85853b29e7
Amar2798 <anupamahg1@gmail.com>
2019-09-21 11:16:30 +0530

    add logic for adding notes and removing when complete

---
+++ index.js
@@ -0,0 +1,44 @@
+var counter = 0;
+
+function handleButton(index) {
+    document.getElementById(index).remove();
+}
+
+function getCardHTML(title, contents) {
+    return '<div class="card my-1">\
+                <div class="card-body">\
+                    <h5 class="card-title">' + title + '</h5>\
+                    <p class="card-text">' + contents + '</p>\
+                    <div class="text-center">\
+                    <button type="button" onclick="handleButton(' + counter + ')" class="btn btn-outline-danger">Complete</button>\
+                    </div>\
+                </div>\
+            </div>';
+}
+
+function createCard(title, contents) {
+    var newCard = document.createElement('div');
+    newCard.classList.add('col-4');
+    newCard.id = counter;
+    document.getElementById('row').appendChild(newCard);
+    document.getElementById(newCard.id).innerHTML = getCardHTML(title, contents);
+    counter++;
+}
+
+function handleClick() {
+    var title = document.querySelector('#title').value;
+    var contents = document.querySelector('#contents').value;
+    console.log(title);
+    console.log(contents);
+
+    if(counter == 0) {
+        /* Create new row
+        Then, create card and append */
+        var newRow = document.createElement('div');
+        newRow.classList.add('row');
+        newRow.id = 'row';
+        document.getElementById('cardContents').appendChild(newRow);
+    } 
+
+    createCard(title, contents);
+}
